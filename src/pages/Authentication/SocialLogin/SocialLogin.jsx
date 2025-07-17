5e754cd27a6ec5d17b1cc7186ea470a2512344c5
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2025-07-17 09:07:32 -0500

    update social login

--- src/pages/Authentication/SocialLogin/SocialLogin.jsx
+++ src/pages/Authentication/SocialLogin/SocialLogin.jsx
@@ -2,32 +2,41 @@ import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import useAuth from '../../../hooks/useAuth';
 import { useLocation, useNavigate } from 'react-router';
+import useAxios from '../../../hooks/useAxios';
 
 const SocialLogin = () => {
+    const { signInWithGoogle } = useAuth();
+    const location = useLocation();
+    const navigate = useNavigate();
+    const form = location.state?.from || '/';
+    const axiosInstance = useAxios();
 
+    const handleGoogleLogin = async () => {
+        try {
+            const result = await signInWithGoogle();
+            const user = result.user;
 
-const { signInWithGoogle} =useAuth();
-const location = useLocation();
-const navigate = useNavigate();
-const form= location.state?.from || '/';
+            const userInfo = {
+                email: user.email,
+                role: 'user', // default role
+                name: user.displayName,
+                photoURL: user.photoURL || '',
+                created_at: new Date().toISOString(),
+                last_login: new Date().toISOString(),
+                isPremium: false
+            };
 
+            const res = await axiosInstance.post('/users', userInfo);
+            console.log('User update info', res.data);
 
-    const handleGoogleLogin = () => {
-        // Handle Google login here
-        signInWithGoogle()
-            .then((result) => {
-                console.log(result.user);
-                navigate(form);
-
-            })
-            .catch((error) => {
-                // Handle login error
-                console.log(error);
-            })
+            navigate(form);
+        } catch (error) {
+            console.error('Google login failed:', error);
+        }
     };
+
     return (
         <div>
-            {/* Google Register Button */}
             <button
                 onClick={handleGoogleLogin}
                 className="mt-4 flex items-center justify-center gap-2 w-full border py-2 rounded-md hover:bg-gray-50 transition"
@@ -35,7 +44,6 @@ const form= location.state?.from || '/';
                 <FcGoogle size={22} />
                 <span className="text-sm font-medium text-gray-700">Login with Google</span>
             </button>
-            
         </div>
     );
 };
