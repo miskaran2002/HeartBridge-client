0f0f0431c1a5900ebc6e0732cdad38e10937e645
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2025-07-18 04:43:30 -0500

    update useaxiossecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,13 +1,42 @@
+// src/hooks/useAxiosSecure.js
 import axios from 'axios';
-import React from 'react';
+import { useEffect } from 'react';
+import useAuth from './useAuth';
 
-const axiosSecure= axios.create({
-    baseURL:`http://localhost:5000`
-})
+const axiosSecure = axios.create({
+    baseURL: 'http://localhost:5000',
+});
+
+let interceptorId;
 
 const useAxiosSecure = () => {
+    const { user } = useAuth();
+
+    useEffect(() => {
+        const attachInterceptor = async () => {
+            if (user?.email) {
+                const token = await user.getIdToken();
+
+                // Remove previous interceptor if exists
+                if (interceptorId !== undefined) {
+                    axiosSecure.interceptors.request.eject(interceptorId);
+                }
+
+                // Attach new one
+                interceptorId = axiosSecure.interceptors.request.use(
+                    (config) => {
+                        config.headers.Authorization = `Bearer ${token}`;
+                        return config;
+                    },
+                    (error) => Promise.reject(error)
+                );
+            }
+        };
+
+        attachInterceptor();
+    }, [user]);
+
     return axiosSecure;
-    
 };
 
 export default useAxiosSecure;
